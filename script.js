aae64ceeb1a4b0333132b4f6143554080a93886c
Deka Thomas <dekacurry30@gmail.com>
2020-05-10 13:24:06 +0700

    Lazy Loading adding
    Adding lazy load into my profile image

--- script.js
+++ script.js
@@ -54,32 +54,36 @@ skills.forEach(skill => {
 })
 
 // Lazy load
-let lazyImages = [...document.querySelectorAll('img.lazy')]
-
-const lazyLoad = function() {
-	setTimeout(function() {
-		lazyImages.forEach(function(lazyImage) {
-			if( (lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display != 'none' ) {
-				lazyImage.src = lazyImage.dataset.src
-				lazyImage.classList.remove('lazy')
-
-				lazyImages = lazyImages.filter(function(image) {
-					return image != lazyImage
-				})
-
-				if (lazyImages.length === 0) {
-					document.removeEventListener("scroll", lazyLoad)
-					window.removeEventListener("resize", lazyLoad)
-					window.removeEventListener("orientationchange", lazyLoad)
+document.addEventListener("DOMContentLoaded", function() {
+	let lazyImages = [...document.querySelectorAll('img.lazy')]
+	
+	const lazyLoad = function() {
+		setTimeout(function() {
+			lazyImages.forEach(function(lazyImage) {
+				if( (lazyImage.getBoundingClientRect().top <= window.innerHeight && lazyImage.getBoundingClientRect().bottom >= 0) && getComputedStyle(lazyImage).display != 'none' ) {
+					lazyImage.src = lazyImage.dataset.src
+					lazyImage.classList.remove('lazy')
+	
+					lazyImages = lazyImages.filter(function(image) {
+						return image != lazyImage
+					})
+	
+					if (lazyImages.length === 0) {
+						document.removeEventListener("scroll", lazyLoad)
+						window.removeEventListener("resize", lazyLoad)
+						window.removeEventListener("orientationchange", lazyLoad)
+					}
 				}
-			}
-		})
-	}, 200)
-}
+			})
+		}, 200)
+	}
 
-document.addEventListener("scroll", lazyLoad)
-window.addEventListener("resize", lazyLoad)
-window.addEventListener("orientationchange", lazyLoad)
+	lazyLoad()
+	
+	document.addEventListener("scroll", lazyLoad)
+	window.addEventListener("resize", lazyLoad)
+	window.addEventListener("orientationchange", lazyLoad)
+})
 
 // Navigation
 const nav = document.querySelector('nav')
